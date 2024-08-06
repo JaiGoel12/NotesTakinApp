23d05b6362164320eb264539f10dcdd3b9aa1e8b
JaiGoel12 <jaigoel1201@gmail.com>
2024-08-06 11:34:36 +0530

    fixing some bugs

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = express();
 const path = require('path');
 const fs=require('fs');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,"public")));
 
 
-app.get("/",(req,res)=>{
-    fs.readdir('./files',(err,files)=>{
-        res.render("index",{files:files});
 
+app.get("/", (req, res) => {
+    fs.readdir('./files', (err, files) => {
+        if (err) {
+            return res.status(500).send("Unable to read files");
+        }
+        res.render("index", { files: files });
     });
-})
+});
 
 app.get("/files/:filename",function(req,res){
     fs.readFile(`./files/${req.params.filename}` ,"utf-8", function(err, filedata){
